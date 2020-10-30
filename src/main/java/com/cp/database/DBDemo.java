943df9bb2b9961d0059f8a1dcaaaabb67af87bed
akinator001 <aakashgandhi1279@gmail.com>
2020-10-30 22:55:56 +0530

    [aakash] Read Data From DataBase

--- src/main/java/com/cp/database/DBDemo.java
+++ src/main/java/com/cp/database/DBDemo.java
@@ -3,12 +3,18 @@ package com.cp.database;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class DBDemo {
 	static String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 	static String username = "root";
-	static String password = "Ashutosh@23";
+	static String password = "Aakash@123";
 
 	public static void main(String[] args) {
 
@@ -39,5 +45,27 @@ public class DBDemo {
 			Driver driverClass = (Driver) driverList.nextElement();
 			System.out.println("  " + driverClass.getClass().getName());
 		}
+	}
+	
+	public static List<EmployeePayrollData> readDataFromDB() {
+		String sql = "SELECT * FROM employee_payroll;";
+		List<EmployeePayrollData> employeePayrollList = new ArrayList();
+		try {
+			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while (result.next()) {
+				int id = result.getInt("id");
+				String name = result.getString("name");
+				double basic_pay = result.getDouble("basic_pay");
+				LocalDate startDate = result.getDate("start").toLocalDate();
+				employeePayrollList.add(new EmployeePayrollData(id, name, basic_pay, startDate));
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+
 	}
 }
